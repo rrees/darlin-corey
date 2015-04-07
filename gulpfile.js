9f292ee9efdd3785cdf647c97009bc0486dbed6e
Robert Rees <shudderfix@gmail.com>
2015-04-07 23:13:50 +0100

    Initial prototype

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var gulpPlugins = require('auto-plug')('gulp');
+var jade = require('jade');
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['jade', 'sass', 'js']);
+
+gulp.task('jade', function() {
+    return gulp
+        .src('templates/**/*.jade')
+        .pipe(gulpPlugins.jade({
+            jade: jade,
+            pretty: true
+            }))
+        .pipe(gulp.dest('build/'))
+});
+
+gulp.task('sass', function() {
+    return gulp.src('scss/**/*.scss')
+        .pipe(gulpPlugins.sass())
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('js', ['bundle']);
+
+gulp.task('ts', function() {
+    return gulp.src('src/*.ts')
+        .pipe(gulpPlugins.typescript({
+            declarationFiles: true,
+            noExternalResolve: true,
+            module: 'commonjs'
+        }))
+        .pipe(gulp.dest('lib'))
+});
+
+gulp.task('jsx', function() {
+    return gulp.src('src/*.jsx')
+        .pipe(gulpPlugins.react())
+        .pipe(gulp.dest('lib'))
+})
+
+gulp.task('bundle', ['ts', 'jsx'], function() {
+    return gulp.src('lib/app.js')
+        .pipe(gulpPlugins.browserify())
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('deploy', ['build'], function() {
+    return gulpPlugins.run('surge build darlin-corey.surge.sh').exec();
+});
---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+
+React.render(
+    <h2>Hello from React</h2>, document.getElementById('app')
+    );
