160b23270346690edf46a8243582e1f51e9e2bf8
Robert Rees <shudderfix@gmail.com>
2015-04-08 08:21:11 +0100

    Creates a component rather than rendering directly

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
+var classnames = require('classnames');
+
+var FlippableCard = React.createClass({
+
+	render: function() {
+		var cardClasses = classnames('card');
+
+		return (
+			    <section className="container">
+			    <div id="card" className="{cardClasses}">
+			    	<figure className="front">This is the front</figure>
+			    	<figure className="back">This is the back</figure>
+			    </div>
+			    </section>
+			);
+	}
+	});
 
 React.render(
-    <section className="container">
-    <div id="card" className="card">
-    	<figure className="front">This is the front</figure>
-    	<figure className="back">This is the back</figure>
-    </div>
-    </section>,
-
-    document.getElementById('app')
+	<FlippableCard />, document.getElementById('app')
     );
