e455572905005c10bd410ca3b8645138d6c60548
Robert Rees <shudderfix@gmail.com>
2015-04-08 08:34:10 +0100

    Card faces are now components

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,14 @@
 var React = require('react');
 var classnames = require('classnames');
 
+var CardFace = React.createClass({
+	render: function() {
+		var classNames = classnames('face', this.props.face);
+
+		return (<figure className={classNames}>{this.props.content}</figure>);
+	}
+});
+
 var FlippableCard = React.createClass({
 
 	render: function() {
@@ -8,9 +16,9 @@ var FlippableCard = React.createClass({
 
 		return (
 			    <section className="container">
-			    <div id="card" className="{cardClasses}">
-			    	<figure className="front">This is the front</figure>
-			    	<figure className="back">This is the back</figure>
+			    <div id="card" className={cardClasses}>
+			    	<CardFace face="front" content="This is the front" />
+			    	<CardFace face="back" content="This is the back" />
 			    </div>
 			    </section>
 			);
