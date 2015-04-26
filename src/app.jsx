e57d7da570b6ce2ea4828a3a140cef81b5835a85
Robert Rees <shudderfix@gmail.com>
2015-04-26 20:34:14 +0100

    Adds question identifiers to the cards and corrects the colours applied to the cards

--- src/app.jsx
+++ src/app.jsx
@@ -21,7 +21,7 @@ var FlippableCard = React.createClass({
 	},
 
 	render: function() {
-		var cardClasses = classnames('card', {'flipped': this.state.flipped});
+		var cardClasses = classnames('card', this.props.questionId, {'flipped': this.state.flipped});
 
 		return (
 			    <section className="container">
@@ -37,7 +37,7 @@ var FlippableCard = React.createClass({
 var Question = React.createClass({
 	render: function () {
 		return (
-			<FlippableCard front={this.props.question} back={this.props.answer}/>
+			<FlippableCard questionId={this.props.questionId} front={this.props.question} back={this.props.answer}/>
 		)
 	}
 });
@@ -53,16 +53,19 @@ var Game = React.createClass({
 
 		var questions = [
 			{
+				id: 'q1',
 				question: "Does Cora Smaw love you?",
 				order: 1,
 				answer: "Answer 1"
 			},
 			{
+				id: 'q2',
 				question: "Who shot Cora?",
 				order: 2,
 				answer: "Answer 2"
 			},
 			{
+				id: 'q3',
 				question: "Will you kill Belt or will he kill you?",
 				order: 3,
 				answer: "Answer 3"
@@ -73,7 +76,7 @@ var Game = React.createClass({
 			<article>
 			{
 				questions.map(function(question, idx) {
-					return <Question key={question.order} question={question.question} answer={question.answer}/>
+					return <Question key={question.order} questionId={question.id} question={question.question} answer={question.answer}/>
 				})
 			}
 			</article>
